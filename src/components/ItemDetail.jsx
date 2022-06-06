8650609cf2343933ae22d1a52c0ad2b643018abf
Daniel Jimenez <barnique2802@gmail.com>
2022-06-06 19:03:11 -0300

    Agregue las rutas para los detalles de los Items

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,5 +1,7 @@
-const ItemDetail = () =>{
-
+const ItemDetail = ({product}) =>{
+    return (
+        <h2>{product.name}</h2>
+    )
 }
 
 export default ItemDetail
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react"
+import { getProducts, getProductById } from "../asyncMock"
+import ItemDetail from './ItemDetail'
+import { useParams } from 'react-router-dom'
+
 const ItemDetailContainer = () =>{
 
+    const [product, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const { productId } = useParams()
+    
+
+    useEffect(() => {
+        setLoading(true)
+            getProductById(productId).then(res => {
+                setProducts(res)
+            }).catch(error => {
+                console.log(error)
+            }).finally(() => {
+                setLoading(false)
+            })
+        
+        
+    }, [productId])
+
+    if(loading) {
+        return <h1>Cargando...</h1>
+    }
+
     return(
-        <h1>Product detail</h1>
+        <>
+            <h1>Product detail</h1>
+            <ItemDetail product={product}/>
+        </>
+        
     )
 }
 
